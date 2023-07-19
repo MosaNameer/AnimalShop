2ebb43bacae71d9281defe749e0327a9f55d5172
MosaNameer <mosa.mn777@gmail.com>
2023-07-19 21:57:14 +0300

    add

---
+++ resources/views/test.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name"><br>
+        <label for="stage">Stage:</label><br>
+        <input type="number" id="stage" name="stage"><br><br>
+        <input type="submit" value="Enter">
+      </form> 
+</body>
+</html>
