db0b165ccb7477c2dca51bd7ea447c2e26e41a1f
MosaNameer <mosa.mn777@gmail.com>
2023-07-20 17:02:22 +0300

    add

---
+++ database/migrations/2023_07_20_134344_create_pets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name")->nullable();
+            $table->integer("age")->nullable();
+            $table->integer("price")->nullable();
+            $table->string("gender")->nullable(); # 1 for male and 2 for female
+            $table->string("breed")->nullable();
+            $table->string("color");
+            $table->string("image")->nullable();
+            $table->string("status")->nullable(); # 1 for sale, 2 sold, 3 for adoption,  1 adopted
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pets');
+    }
+};
