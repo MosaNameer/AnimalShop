1d10b31fd12777d0d57511d2e8f161025a3f3ee8
MosaNameer <mosa.mn777@gmail.com>
2023-07-21 00:54:21 +0300

    add

--- resources/views/livewire/pages/home/pets-card.blade.php
+++ resources/views/livewire/pages/home/pets-card.blade.php
@@ -1,12 +1,29 @@
-<section class="splide" aria-labelledby="carousel-heading">
-    <h2 id="carousel-heading">Splide Basic HTML Example</h2>
-
-
-    <div class="splide__track">
-          <ul class="splide__list">
-              <li class="splide__slide">Slide 01</li>
-              <li class="splide__slide">Slide 02</li>
-              <li class="splide__slide">Slide 03</li>
-          </ul>
+    <div
+        class="border-gray-100/30 flex w-full max-w-xs flex-col self-center overflow-hidden rounded-lg border bg-gray-100 shadow-md">
+        <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+            <img class="peer absolute top-0 right-0 h-full w-full object-cover"
+                src="{{ asset('img/cat1.jpg') }}"
+                alt="product image" />
+            <!-- <span class="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-gray-800">39% OFF</span> -->
+        </a>
+        <div class="mt-4 px-5 pb-5">
+            <a href="#">
+                <h5 class="text-xl tracking-tight text-gray-800">Nike Air MX Super 2500 - Red</h5>
+            </a>
+            <div class="mt-2 mb-5 flex items-center justify-between">
+                <p>
+                    <span class="text-3xl font-bold text-gray-800">$449</span>
+                    {{-- discount --}}
+                    {{-- <span class="text-sm text-gray-800 line-through">$699</span> --}}
+                </p>
+            </div>
+            <a href="#"
+                class="hover:border-white/40 flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-gray-800 focus:outline-none focus:ring-4 focus:ring-blue-300">
+                <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                </svg>
+                buy</a>
+        </div>
     </div>
-  </section>
