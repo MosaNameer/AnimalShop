d67becca268572b5e8fd84cebf84d4bd0128371e
MosaNameer <mosa.mn777@gmail.com>
2023-07-24 17:21:19 +0300

    add

--- resources/views/livewire/pages/about-us/main.blade.php
+++ resources/views/livewire/pages/about-us/main.blade.php
@@ -1,4 +1,9 @@
 <div>
     {{-- Be like water. --}}
-    hello from about us page
+    <h1 class="text-center text-4xl font-semibold mt-40  ">
+        جميع الحقوق محفوضة لشركة تقنونة
+    </h1>
+    <h1 class="text-center text-2xl font-semibold mt-5">
+        07732323440
+    </h1>
 </div>
--- resources/views/livewire/pages/category/main.blade.php
+++ resources/views/livewire/pages/category/main.blade.php
@@ -1,3 +1,9 @@
 <div>
-    hello from category page
+    <h1 class="font-semibold text-3xl mt-10 ml-16">
+        what kind of pet do you want ?
+    </h1>
+
+    <div class="ml-16">
+        
+    </div>
 </div>
