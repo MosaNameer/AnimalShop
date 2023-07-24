b3d18a4e1a7c5679b75a61be1e1827b540e32855
MosaNameer <mosa.mn777@gmail.com>
2023-07-24 20:18:24 +0300

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,46 @@
-<html>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<head>
-    @livewireStyles
-    @vite('resources/css/app.css')
-    <link href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css" rel="stylesheet">
-</head>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-<body class="bg-gradient-to-br from-pink-300 via-purple-300 to-indigo-400">
-    <div>
-        @livewire('components.navbar.main')
-    </div>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <main>
-        {{ $slot }}
-    </main>
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    @livewire('components.footer')
+        <!-- Styles -->
+        @livewireStyles
+    </head>
+    <body class="font-sans antialiased">
+        <x-banner />
 
-    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js"></script>
-    @livewireScripts
-</body>
+        <div class="min-h-screen bg-gray-100">
+            {{-- @livewire('navigation-menu') --}}
+            @livewire('components.navbar.main')
 
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+
+        @stack('modals')
+
+        @livewireScripts
+    </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,22 @@ use App\Http\Livewire\Pages\Pet\Main as Pet;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get("/home", Home::class)->name("home");
 Route::get("/category", Category::class)->name("category");
 Route::get("/about", About::class)->name("about");
 Route::get("/pet", Pet::class)->name("pet");
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
